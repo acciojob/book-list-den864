05a26232267a64e740b0777f5b48e339a8ef0c0a
Dhaval Birade <69907022+den864@users.noreply.github.com>
2025-09-09 13:26:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,56 @@
-//your JS code here. If required.
+
+const btn=document.querySelector(".add-book-btn");
+
+btn.addEventListener("click",(event)=>{
+    event.preventDefault();
+
+    const tr=document.createElement("tr");
+
+    const form=document.querySelector("form");
+
+    const title=form.title.value.trim();
+    const author=form.author.value.trim();
+    const isbn=form.isbn.value.trim();
+
+    if(!title || !author || !isbn){
+        alert("Insert Required Fields");
+        return;
+    }
+
+
+    const data=[
+        {name:title,className:"title-table"},
+        {name:author,className:"author-table"},
+        {name:isbn,className:"isbn-table"}
+    ];
+
+    data.forEach((ele,index)=>{
+        const td=document.createElement("td");
+        td.textContent=ele.name;
+        td.classList.add(ele.className);
+        tr.appendChild(td);
+    })
+
+
+
+    const actionTd=document.createElement("td");
+
+    const deleteBtn=document.createElement("button");
+    deleteBtn.textContent = "❌";
+    deleteBtn.classList.add("delete-btn");
+
+    deleteBtn.addEventListener("click",()=>{
+        tr.remove();
+    })
+
+
+    actionTd.appendChild(deleteBtn);
+
+    tr.appendChild(actionTd);
+
+    document.querySelector("#book-list").appendChild(tr);
+
+    form.reset();
+
+})
+ 
